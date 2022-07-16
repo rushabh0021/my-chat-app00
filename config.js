7a3b6e8794a8c9762784c0c4a365f08764b0cc9e
Rushabh Modi <rushabh374@gmail.com>
2022-07-16 18:47:15 +0530

    changed db config url

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var dbUrl = "mongodb+srv://database-user:ISe54HlKIcSequPM@cluster0.dryva.mongodb.net/merng?retryWrites=true&w=majority";
+var dbUrl = `mongodb+srv://database-user:${process.env.dbUserPass}@cluster0.dryva.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = dbUrl;
